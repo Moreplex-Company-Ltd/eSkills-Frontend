56d5f060e7898ece34fab14edad4da6015db96a0
johnkdunyo <johnkdunyo@gmail.com>
2022-07-09 12:26:20 +0000

    added course categories based on interests, made them responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CourseCard from '../components/CourseCard';
+import CoursesRowByCategory from '../components/CoursesRowByCategory';
 import Footer from '../components/Footer';
 
 
@@ -9,16 +9,17 @@ import NavbarSignIn from '../components/navbars/NavbarSignIn';
 const Home = () => {
   return (
     <React.Fragment>
-        <NavbarSignIn />
+      <NavbarSignIn />
 
-        <section>
+
+      <section className='bg-grayBackground'>
           {/* desktop */}
-          <div className="h-[680px] hidden sm:flex flex-col hero-section-main justify-center items-center ">
+          <div className="h-[400px] hidden sm:flex flex-col hero-section-main justify-center items-center ">
                 <div className=" p-4 text-white justify-center">
                 <h1 className="text-2xl sm:text-4xl font-bold sm:font-bold text-center text-gold">Inspirations Starts Here</h1>
-                    <p className="text-lg sm:text-2xl text-center text-white mt-5">
-                        Let's start with these. We hope you love them <br/>
-                        We will continue to recommend more as you journey with us
+                    <p className="text-xl font-medium sm:text-2xl text-center text-white mt-5 px-20">
+                    Explore new skills, deepen existing passions, and get lost in creativity. 
+                    <br/>What you find just might surprise and inspire you.
                     </p>
                 </div>
                 {/* <div className='flex justify-center gap-10'>
@@ -31,11 +32,10 @@ const Home = () => {
 
             {/* mobile */}
           <div className="h-[250px] sm:hidden flex hero-section-main justify-center items-center">
-                <div className=" p-4 mt-5 text-white justify-center">
-                <h1 className="text-xl sm:text-4xl font-bold sm:font-bold text-center text-gold">Inspirations Starts Here</h1>
-                    <p className="text-sm text-center text-white mt-5">
-                        Let's start with these. We hope you love them <br/>
-                        We will continue to recommend more as you journey with us
+                <div className=" p-4 mt-14 text-white justify-center">
+                <h1 className="text-2xl sm:text-4xl font-bold sm:font-bold text-center text-gold">Inspirations Starts Here</h1>
+                    <p className="text-lg text-center font-normal text-white mt-5">
+                    Explore new skills, deepen existing passions, and establish yourself
                     </p>
                 </div>
                 {/* <div className='flex justify-center gap-10'>
@@ -44,24 +44,24 @@ const Home = () => {
                   <CourseCard />
                 </div> */}
             </div>
-
-
         </section>
 
+        
+        <CoursesRowByCategory 
+          title='Because you love Cosmetic Manufacturing'
+        />
 
-        <section className='p-10 flex flex-col items-center'>
-          <div className='flex flex-wrap gap-6'>
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
-          </div>
+        <CoursesRowByCategory 
+          title='Establish your Business: Marketing Courses to take you to the next level'
+        />
 
+     
+      
+
+  
         
 
-        
-
-        </section>
+       
 
 
 
