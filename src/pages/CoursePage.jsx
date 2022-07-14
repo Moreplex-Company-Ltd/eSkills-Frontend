c47d0d400838813dd76813b989e0d433f0ad74c5
johnkdunyo <johnkdunyo@gmail.com>
2022-07-14 12:54:17 +0000

    updated carouse, and added course detail page

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 import NavbarSignedIn from '../components/navbars/NavbarSignedIn'
 
 
 const courseTitle = 'Introduction to Soap Branding: Branding the Perfect Soap'
-const courseAuthor = 'Prince Baff'
+// const courseAuthor = 'Prince Baff'
 
 
 const CoursePage = () => {
@@ -13,9 +13,9 @@ const CoursePage = () => {
         <NavbarSignedIn />
 
         <section className='h-full w-full pt-16'>
-            <div className='border pt-16 border-red-700'>
-                <div className='container mx-auto'>
-                    <h1 className='mb-3 text-2xl tracking-tight font-bold text-primaryBlue  md:text-4xl'>{courseTitle}</h1>
+            <div className='border pt-16 border-red-700 bg-gray-600'>
+                <div className=''>
+                    <h1 className='mb-3 text-2xl tracking-tight font-bold text-primaryBlue  ml-10 md:text-4xl'>{courseTitle}</h1>
                     <hr />
                     <div className='flex justify-between mt-5'>
                         <button className='px-4 py-1 rounded bg-gray-100 border border-secondaryBlue hover:bg-gray-200'>Prev</button>
